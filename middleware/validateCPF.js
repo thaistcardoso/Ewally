f1f7486abfe77569afe2c83d2037be7d473c4135
Thais Tauane Cardoso <65881471+thaistcardoso@users.noreply.github.com>
2024-01-01 20:42:05 -0300

    Delete validateCPF.js
    Deletando arquivos desnecessário

--- middleware/validateCPF.js
+++
@@ -1,5 +0,0 @@
-function validateCpf(req, res, next) {
-    const { CPF } = req.body;
-    if (CPF.length != 11) return res.status(400).json({ message: 'CPF precisa conter 11 digitos' });
-    let findFriend = [];
-};
